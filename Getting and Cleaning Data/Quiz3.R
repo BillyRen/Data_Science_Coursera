d50ba5f381abb72cc0c57620eb395718a0cef07a
Ren Cheng-Bin <renchengbin.billy@gmail.com>
2014-04-28 22:53:59 +0800

    Add  Readme and CodeBook

--- Getting and Cleaning Data/Quiz3.R
+++ Getting and Cleaning Data/Quiz3.R
@@ -2,3 +2,7 @@ img <- readJPEG("C:/Users/Billy/SkyDrive/Desktop/getdata-jeff (1).jpg",native=TR
 
 
 getdata.data.GDP <- read.csv("C:/Users/Billy/SkyDrive/Desktop/getdata-data-GDP.csv")
+
+temp<-merge.data[order(merge.data$Ranking),c("Income.Group","Ranking")]
+temp<-temp[1:38,]
+length(which(temp$Income.Group=="Lower middle income"))
